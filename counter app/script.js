93a845c6bd17a522e106389bd3a75a19726afb8c
AKSHAT BIRLE <birleakshat0@gmail.com>
2025-07-16 11:55:32 +0530

    Add files via upload

---
+++ counter app/script.js
@@ -0,0 +1,10 @@
+let number = 0;
+
+function addition() {
+  number++;
+  document.getElementById("numberhai").innerText = number;
+}
+function subtraction() {
+  number--;
+  document.getElementById("numberhai").innerText = number;
+}
